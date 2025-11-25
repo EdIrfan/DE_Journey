97f6195349f7892dd250e4e33a0432fa29ca13b4
EdIrfan <60884742+EdIrfan@users.noreply.github.com>
2025-11-25 23:32:43 +0530

    ok newfiles

---
+++ Sprints/Sprint1/Exceptions2_hardcore.py
@@ -0,0 +1,49 @@
+#https://docs.python.org/3/tutorial/errors.html#handling-exceptions last example
+
+def f():
+    raise OSError('operation failed')
+
+excs = []
+for i in range(3):
+    try:
+        f()
+    except Exception as e:
+        e.add_note(f'Happened in Iteration {i+1}')
+        excs.append(e)
+
+raise ExceptionGroup('We have some problems', excs)
+
+# An ExceptionGroup is raised containing all the exceptions that occurred in the loop.
+
+#+ Exception Group Traceback (most recent call last):
+#   |   File "<stdin>", line 1, in <module>
+#   |     raise ExceptionGroup('We have some problems', excs)
+#   | ExceptionGroup: We have some problems (3 sub-exceptions)
+#   +-+---------------- 1 ----------------
+#     | Traceback (most recent call last):
+#     |   File "<stdin>", line 3, in <module>
+#     |     f()
+#     |     ~^^
+#     |   File "<stdin>", line 2, in f
+#     |     raise OSError('operation failed')
+#     | OSError: operation failed
+#     | Happened in Iteration 1
+#     +---------------- 2 ----------------
+#     | Traceback (most recent call last):
+#     |   File "<stdin>", line 3, in <module>
+#     |     f()
+#     |     ~^^
+#     |   File "<stdin>", line 2, in f
+#     |     raise OSError('operation failed')
+#     | OSError: operation failed
+#     | Happened in Iteration 2
+#     +---------------- 3 ----------------
+#     | Traceback (most recent call last):
+#     |   File "<stdin>", line 3, in <module>
+#     |     f()
+#     |     ~^^
+#     |   File "<stdin>", line 2, in f
+#     |     raise OSError('operation failed')
+#     | OSError: operation failed
+#     | Happened in Iteration 3
+#     +------------------------------------
