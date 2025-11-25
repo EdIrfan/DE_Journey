df5eb0eb5d83e9f61e7189eb779a70dbdb532bab
EdIrfan <60884742+EdIrfan@users.noreply.github.com>
2025-11-25 23:26:59 +0530

    NEW FILES AND WORKSPACE CHANGES:

---
+++ Sprints/Sprint1/parameters.py
@@ -0,0 +1,19 @@
+#This program here will show how parameters can be passed in many effective ways.
+#return true for magicnumber being 7 else false.
+
+def foo(a, b, c, *args):
+    return len(args)
+
+def bar(a, b, c, **kwargs):
+    return kwargs["magicnumber"] == 7
+
+
+# test code
+if foo(1, 2, 3, 4) == 1:
+    print("Good.")
+if foo(1, 2, 3, 4, 5) == 2:
+    print("Better.")
+if bar(1, 2, 3, magicnumber=6) == False:
+    print("Great.")
+if bar(1, 2, 3, magicnumber=7) == True:
+    print("Awesome!")
