b86b5b40295268d054512349839fba095e6bda35
EdIrfan <60884742+EdIrfan@users.noreply.github.com>
2025-11-25 17:26:00 +0530

    New files added for Sprint 1 tasks.

---
+++ Sprints/Sprint1/Generators.py
@@ -0,0 +1,22 @@
+# fill in this function
+def fib():
+    a, b = 0, 1
+    while 1:
+        yield a
+        a, b = b, a + b
+
+# testing code
+import types
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
+
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
+
+#this is fibonacci generator and the fact here works that each time we call next() on the generator it resumes from where it last yielded a value.
+#Yield basically allows the function to produce a series of values over time, pausing after each yield and resuming from that point on the next call.
+#Thus it is helpful for saving computation and memory when dealing with large sequences.
---
+++ Sprints/Sprint1/lambda.py
@@ -0,0 +1,8 @@
+#Write a program using lambda functions to check if a number in the given list is odd. Print "True" if the number is odd or "False" if not for each element.
+#The work of a lambda function simple. func_name = input : output
+
+l = [2,4,7,3,14,19]
+for i in l:
+    # your code here
+    my_lambda = lambda x : (x % 2) == 1
+    print(my_lambda(i))
